831756987625582d3babb3a4548d5a09fd20272a
Etienne Lawlor <etienne@shopsavvy.com>
2015-02-01 09:52:54 -0800

    Bug fix

--- app/src/main/java/com/etiennelawlor/quickreturn/itemdecorations/SpacesItemDecoration.java
+++ app/src/main/java/com/etiennelawlor/quickreturn/itemdecorations/SpacesItemDecoration.java
@@ -2,6 +2,7 @@ package com.etiennelawlor.quickreturn.itemdecorations;
 
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -21,7 +22,7 @@ public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
             outRect.right = space;
             outRect.top = space;
             outRect.bottom = space/2;
-        } else if (parent.getChildPosition(view) == parent.getChildCount()-1){
+        } else if (parent.getChildPosition(view) == parent.getLayoutManager().getItemCount()-1){
             outRect.left = space;
             outRect.right = space;
             outRect.top = space/2;
